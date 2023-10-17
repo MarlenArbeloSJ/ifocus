9446b3bec280be912142f94a18c11bc592df2fdc
Marlen Arbelo <marlenarbelo@sjnative.com>
2023-10-17 22:57:57 +0200

    Trying to add carousel

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  /*   constructor(){
+    const myCarouselElement = document.querySelector('#carousel')
 
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
+  } */
 }
