95f4e756ab21c4eab0d9114b62ee0273b798e553
Marlen Arbelo <marlenarbelo@sjnative.com>
2023-10-17 19:14:33 +0200

    Control bad credentials

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -6,6 +6,7 @@ import { LoginForm } from 'src/app/interfaces/login-form.interface';
 import { User } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -48,7 +49,14 @@ export class LoginPageComponent {
       this.authService.saveToken(response.access_token);
       let user = this.authService.user;
       window.alert('Bienvenido ' + user.name);
-    })
+    }, (error) => {
+      if(error.status == 400){
+        Swal.fire({
+          icon: 'error',
+          title: 'Credenciales incorrectas'
+        })
+      }
+    });
 
   }
 }
